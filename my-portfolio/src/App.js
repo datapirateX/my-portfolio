445726fbe996eb83d48aaa3e3d1c5e9fc258a4b5
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-11-22 23:56:19 +0600

    uploaded assets file

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,19 @@
+import Navbar from "./scenes/Navbar";
 
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
   return (
-    <div>
-      
-    </div>
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      </div>
   );
 }
 

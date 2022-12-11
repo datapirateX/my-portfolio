dd0ff618d53877d0b0ae1af7d31e76d78c646ffa
datapirateX <67560590+datapirateX@users.noreply.github.com>
2022-12-11 16:26:19 +0600

    testimonial text changes

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Testimonials = () => {
   return (
-    <section id="testimonials" className="pt-24 pb-16">
+    <section id="testimonials" className="pt-32 pb-16">
       {/* HEADING */}
       <motion.div
         className="md:w-1/3 text-center md:text-left"
@@ -16,20 +16,21 @@ const Testimonials = () => {
           visible: { opacity: 1, x: 0 },
         }}
       >
-        <p className="font-playfair font-semibold text-4xl mb-2 text-red">
+        <p className="font-playfair font-semibold text-4xl mb-5 text-red">
           TESTIMONIALS
         </p>
         <LineGradient width="mx-auto w-2/5" />
         <p className="mt-10">
-          Here's What People are Saying About My Work. 
+          Here's What People are Saying About My Work. Aliquam aliquet integer
+          ut fames odio in at. At magna ornare dictum lectus.
         </p>
       </motion.div>
 
       {/* TESTIMONIALS */}
       <div className="md:flex md:justify-between gap-8">
         <motion.div
-          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-60
-            before:absolute before:top-[-210px] before:-ml-[110px] before:left-1/2 before:content-person1"
+          className="mx-auto relative bg-blue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person1"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -39,15 +40,15 @@ const Testimonials = () => {
             visible: { opacity: 1, scale: 1 },
           }}
         >
-          
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-          "He was always a great coworker, very smart guy with a solution-oriented approach to whatever came to us. Thanks to him the company made a huge progress in our app, and I would recommend him to anyone that wants smart people in their team."
+            A auctor pharetra hendrerit mattis amet etiam interdum platea.
           </p>
         </motion.div>
 
         <motion.div
-          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-60
-            before:absolute before:top-[-210px] before:-ml-[110px] before:left-1/2 before:content-person2"
+          className="mx-auto relative bg-red max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person2"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -57,14 +58,16 @@ const Testimonials = () => {
             visible: { opacity: 1, scale: 1 },
           }}
         >
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-          "I was impressed by his ability to handle any situation calmly and patiently, even with the toughest clients. This natural skill of his has helped us grab many clients throughout our journey."
+            Aliquam aliquet integer ut fames odio in at. At magna ornare dictum
+            lectus.
           </p>
         </motion.div>
 
         <motion.div
-          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-60
-            before:absolute before:top-[-210px] before:-ml-[110px] before:left-1/2 before:content-person3"
+          className="mx-auto relative bg-yellow max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person3"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -74,8 +77,9 @@ const Testimonials = () => {
             visible: { opacity: 1, scale: 1 },
           }}
         >
+          <p className="font-playfair text-6xl">“</p>
           <p className="text-center text-xl">
-            "Fames odio in at. At magna ornare dictum lectus."
+            Fames odio in at. At magna ornare dictum lectus.
           </p>
         </motion.div>
       </div>
